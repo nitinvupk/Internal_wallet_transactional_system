1cb22ab25ce47f23c6398ed83529f9f7d88cbfb5
nitinvupk <nitinvupk83@gmail.com>
2023-07-14 19:43:26 +0530

    add model and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([
+  { name: 'User 1', email: 'user1@gmail.com', password: 'password123'},
+  { name: 'User 2', email: 'user2@gmail.com', password: 'password234'},
+  { name: 'User 3', email: 'user3@gmail.com', password: 'password345'}
+])
